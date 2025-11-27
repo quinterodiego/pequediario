a3360f592897d53099980453cc73a81501b78aac
Diego Quintero <d86webs@gmail.com>
2025-11-27 18:18:48 -0300

    corrección cards de planes

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -12,7 +12,6 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
       enableSystem={false}
       disableTransitionOnChange
       storageKey="peque-diario-theme"
-      suppressColorSchemeWarning
     >
       {children}
     </NextThemesProvider>
